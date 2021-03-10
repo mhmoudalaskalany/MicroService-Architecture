50c4ab5d8bea332f0a0dd46b0c1ccb269398c369
Mahmoud Alaskalany <mhmoudalaskalany@gmail.com>
2021-03-10 20:32:49 +0400

    Add project files.

---
+++ APIGetWay/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace APIGetWay
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((host, config) =>
+            {
+                config.AddJsonFile("ocelot.json");
+            })
+            .UseIISIntegration()
+            .UseStartup<Startup>();
+    }
+}
---
+++ BackendCore.Common/DTO/Employee/EmployeeDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using BackendCore.Common.Core;
+
+namespace BackendCore.Common.DTO.Employee
+{
+    public class EmployeeDto : IPrimaryKeyField<Guid?>
+    {
+        public Guid? Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string JobTitle { get; set; }
+        public decimal? Salary { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+}
